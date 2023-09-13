2ccbbe2160a51529dbe291f2dc19946d5ca932fd
MoonRainy21 <shmoon323@gmail.com>
2023-09-13 09:37:31 +0900

    Added Logos and NavBar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,101 @@
+import {
+  Box,
+  Flex,
+  Avatar,
+  Text,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+  Image,
+  useBreakpointValue,
+  Link,
+} from "@chakra-ui/react";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import logo from "@/assets/logo.png";
+import { use } from "react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const NavLink = (props: Props) => {
+  const { children } = props;
+
+  return (
+    <Box
+      as="a"
+      px={2}
+      py={1}
+      rounded={"md"}
+      _hover={{
+        textDecoration: "none",
+        bg: useColorModeValue("gray.200", "gray.700"),
+      }}
+      href={"#"}
+    >
+      {children}
+    </Box>
+  );
+};
+
+export function NavBar() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <Link href="/">
+            {/*<Image src={
+            useColorModeValue("black_logo.png", "gray_logo.png")
+          } alt='logo' width={45} height={50} />*/}
+            <Text
+              textAlign={useBreakpointValue({ base: "center", md: "left" })}
+              fontFamily={"heading"}
+              fontWeight={"extrabold"}
+              color={useColorModeValue("gray.800", "white")}
+            >
+              Yt-Download
+            </Text>
+          </Link>
+
+          <Flex alignItems={"center"}>
+            <Stack direction={"row"} spacing={7}>
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rounded={"full"}
+                  variant={"link"}
+                  cursor={"pointer"}
+                  minW={0}
+                >
+                  <Text
+                    fontFamily={"heading"}
+                    fontWeight={"extrabold"}
+                    color={useColorModeValue("gray.800", "white")}
+                  >
+                    History
+                  </Text>
+                </MenuButton>
+                <MenuList alignItems={"center"}>
+                  <MenuItem>Download History</MenuItem>
+                </MenuList>
+                <Button onClick={toggleColorMode}>
+                  {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+                </Button>
+              </Menu>
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  );
+}
